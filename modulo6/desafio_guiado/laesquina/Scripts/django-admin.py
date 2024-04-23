216c74cea8a52ff35109311c349c2a7b92861537
Jesus <jesus.maldonado.villarroel@gmail.com>
2024-04-22 21:29:26 -0400

    Desafio_Guiado_Terminado

---
+++ modulo6/desafio_guiado/laesquina/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jesus\Desktop\bootcamp-python\modulo6\desafio_guiado\laesquina\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
