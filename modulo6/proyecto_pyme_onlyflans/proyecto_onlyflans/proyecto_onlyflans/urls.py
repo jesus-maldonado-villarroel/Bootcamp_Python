ca1b061951d77a65fd21fe8bbad62d3cc1517076
Jesus <jesus.maldonado.villarroel@gmail.com>
2024-04-26 20:58:30 -0400

    Hito2_Finalizado

--- modulo6/proyecto_pyme_onlyflans/proyecto_onlyflans/proyecto_onlyflans/urls.py
+++ modulo6/proyecto_pyme_onlyflans/proyecto_onlyflans/proyecto_onlyflans/urls.py
@@ -1,3 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from web.views import indice, acerca, bienvenido, base
+
 """
 URL configuration for proyecto_onlyflans project.
 
@@ -14,9 +18,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indice, name="indice"),
+    path('acerca', acerca, name="acerca"),
+    path('bienvenido', bienvenido, name="bienvenido"),
 ]
---
+++ modulo6/proyecto_pyme_onlyflans/proyecto_onlyflans/web/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def indice(request):
+    return render(request, 'index.html', {})
+
+
+def acerca(request):
+    return render(request, 'about.html', {})
+
+
+def bienvenido(request):
+    return render(request, 'welcome.html', {})
+
+
+def base(request):
+    return render(request, 'base.html', {})
