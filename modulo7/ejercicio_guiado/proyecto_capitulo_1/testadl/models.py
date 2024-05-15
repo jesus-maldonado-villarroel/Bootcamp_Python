f14d0be569f3dba2bebea0e916312e0808fb1850
Jesus <jesus.maldonado.villarroel@gmail.com>
2024-05-15 19:55:56 -0400

    ejercicio_guiado

---
+++ modulo7/ejercicio_guiado/proyecto_capitulo_1/testadl/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Persona(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=50)
